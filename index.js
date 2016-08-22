8f6262153aef41aaad14ef7ebbd0d8050f5ef03c
Scott McCormack <scott.mccormack@articulate.com>
2016-08-22 02:30:01 -0400

    Extract puddles and convert to es6 modules

---
+++ index.js
@@ -0,0 +1,5 @@
+export { default as action  } from './lib/action'
+export { default as combine } from './lib/combine'
+export { default as h       } from 'snabbdom/h'
+export { default as handle  } from './lib/handle'
+export { default as mount   } from './lib/mount'
---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import buble       from 'rollup-plugin-buble'
+import commonjs    from 'rollup-plugin-commonjs'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import uglify      from 'rollup-plugin-uglify'
+
+export default {
+  entry: 'index.js',
+  format: 'iife',
+  moduleName: 'puddles',
+  plugins: [
+    commonjs(),
+    nodeResolve({
+      browser: true,
+      jsnext:  true
+    }),
+    buble(),
+    uglify({ mangle: true, compress: true })
+  ]
+}
