dadaa5d43d346099a13507d274174e9f5fb81cde
Scott McCormack <scott.mccormack@articulate.com>
2016-08-24 02:32:18 -0400

    Reorganize route reducer as a duck

---
+++ ducks/route.js
@@ -0,0 +1,16 @@
+const concat = require('ramda/src/concat')
+
+const action = require('../lib/action')
+const handle = require('../lib/handle')
+
+const prefix = concat('puddles/route/')
+
+const ROUTE_CHANGED = prefix('ROUTE_CHANGED')
+
+const reducer = handle({}, {
+  [ ROUTE_CHANGED ]: (_, route) => route
+})
+
+reducer.routeChanged = action(ROUTE_CHANGED)
+
+module.exports = reducer
