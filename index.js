8f90d95769ee907bc67ef9f8ec2cf1bcdfbbf6e8
Scott McCormack <scott.mccormack@articulate.com>
2016-08-24 02:16:13 -0400

    Add router and switch back to common.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-export { default as action  } from './lib/action'
-export { default as combine } from './lib/combine'
-export { default as h       } from 'snabbdom/h'
-export { default as handle  } from './lib/handle'
-export { default as mount   } from './lib/mount'
+exports.action  = require('./lib/action')
+exports.combine = require('./lib/combine')
+exports.h       = require('snabbdom/h')
+exports.handle  = require('./lib/handle')
+exports.mount   = require('./lib/mount')
+exports.route   = require('./lib/route')
