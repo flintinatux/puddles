69f5d4e1b294ddad030fac7610c2a19c6ef6a454
Scott McCormack <scott.mccormack@articulate.com>
2016-09-06 00:57:39 -0400

    Move href to top-level method

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ p.action  = require('./lib/action')
 p.batch   = require('./lib/batch')
 p.combine = require('./lib/combine')
 p.handle  = require('./lib/handle')
+p.href    = require('./lib/href')
 p.mount   = require('./lib/mount')
 p.route   = require('./lib/route')
 
