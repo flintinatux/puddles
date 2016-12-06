d824f9629f16fe2b917901827e2519ad98850046
Scott McCormack <scott.mccormack@articulate.com>
2016-12-06 00:21:12 -0500

    Finish up p.mount specs

--- test/mount-spec.js
+++ test/mount-spec.js
@@ -1,27 +1,36 @@
 /* jshint expr: true */
 
-const { compose } = require('crocks')
-const { expect }  = require('chai')
-const { jsdom }   = require('jsdom')
+const { compose, IO } = require('crocks')
+const { expect } = require('chai')
+const { jsdom }  = require('jsdom')
 const h = require('snabbdom/h')
 
 const action = require('../lib/action')
+const batch  = require('../lib/batch')
 const handle = require('../lib/handle')
 const mount  = require('../lib/mount')
+const Task   = require('../lib/task')
 
 global.document = jsdom()
 
-const raf = fn =>
-  setTimeout(fn, 60)
+const add = action('ADD')
 
 const reducer = handle(0, {
-  INC: (count, step) => count + step
+  ADD: (count, step, error) => error ? step : count + step
 })
+
+const spy = () => {
+  var calls = 0
+  return () => ++calls
+}
 
 const view = state =>
   h('div.foo', {
-    on: { click: [ action('INC'), 2 ] }
+    on: { click: [ add, 2 ] }
   }, state)
+
+const wait = fn =>
+  setTimeout(fn, 60)
 
 describe('p.mount', function () {
   var dispatch, root, state, teardown
@@ -33,19 +42,44 @@ describe('p.mount', function () {
     root      = res.root
     state     = res.state
     teardown  = res.teardown
-    raf(done)
+    expect(state()).to.equal(0)
+    wait(done)
   });
 
-  it('returns dispatch, state, and root vnode streams', function () {
+  it('returns a dispatch stream', function () {
     expect(dispatch).to.be.a('function')
-    expect(root).to.be.a('function')
-    expect(state).to.be.a('function')
-
-    expect(state()).to.equal(0)
-    dispatch(action('INC', 2))
+    dispatch(add(2))
     expect(state()).to.equal(2)
+  });
 
+  it('returns a state stream', function () {
+    expect(state).to.be.a('function')
+    expect(state()).to.equal(0)
+  });
+
+  it('returns a root vnode stream', function() {
+    expect(root).to.be.a('function')
     expect(root().sel).to.equal('div.foo')
+  });
+
+  describe('returns a teardown function, that when called', function () {
+    beforeEach(function () {
+      expect(teardown).to.be.a('function')
+      teardown()
+    });
+
+    it('patches the root to be an empty div', function () {
+      expect(root().sel).to.equal('div')
+      expect(root().children).to.be.undefined
+    });
+
+    it('ends all the streams', function () {
+      expect(dispatch.end()).to.be.true
+      expect(root.end()).to.be.true
+      expect(state.end()).to.be.true
+      dispatch(add(2))
+      expect(state()).to.equal(0)
+    });
   });
 
   it('mounts the view', function () {
@@ -54,17 +88,97 @@ describe('p.mount', function () {
 
   it('redraws after actions are dispatched', function (done) {
     expect(root().text).to.equal(0)
-    dispatch(action('INC', 2))
-    raf(() => {
+    dispatch(add(2))
+    wait(() => {
       expect(root().text).to.equal(2)
       done()
     })
   });
 
+  it('throttles redraws', function (done) {
+    const calls = root.map(spy())
+    expect(calls()).to.equal(1) // to initialize stream
+    dispatch(add(2))
+    dispatch(add(3))
+    wait(() => {
+      expect(calls()).to.equal(2) // only one redraw
+      done()
+    })
+  });
+
   it('composes event handlers with the dispatch function', function () {
-    expect(state()).to.equal(0)
     root().elm.click()
     expect(state()).to.equal(2)
+  });
+
+  it('runs dispatched thunk functions', function (done) {
+    const thunk = (d, s) => {
+      expect(d).to.equal(dispatch)
+      expect(s).to.equal(state)
+      done()
+    }
+    dispatch(thunk)
+  });
+
+  it('unwraps batched actions and dispatches each one', function () {
+    dispatch(batch([ add(2), add(3) ]))
+    expect(state()).to.equal(5)
+  });
+
+  describe('when a forkable payload is resolved', function () {
+    beforeEach(function () {
+      dispatch(add(Task((rej, res) => res(2))))
+    });
+
+    it('dispatches a new action with resolved value', function () {
+      expect(state()).to.equal(2)
+    });
+  });
+
+  describe('when a forkable payload is rejected', function () {
+    const err = new Error('an error')
+
+    beforeEach(function () {
+      dispatch(add(Task(rej => rej(err))))
+    });
+
+    it('dispatches a matching error action', function () {
+      expect(state()).to.equal(err)
+    });
+  });
+
+  describe('when a runnable payload is supplied', function () {
+    beforeEach(function () {
+      dispatch(add(IO(() => 2)))
+    });
+
+    it('runs and dispatches a new action with the result', function () {
+      expect(state()).to.equal(2)
+    });
+  });
+
+  describe('when a thenable payload is resolved', function () {
+    beforeEach(function (done) {
+      dispatch(add(new Promise(res => res(2))))
+      wait(done)
+    });
+
+    it('dispatches a new action with the resolved value', function () {
+      expect(state()).to.equal(2)
+    });
+  });
+
+  describe('when a thenable payload is rejected', function () {
+    const err = new Error('an error')
+
+    beforeEach(function (done) {
+      dispatch(add(new Promise((res, rej) => rej(err))))
+      wait(done)
+    });
+
+    it('dispatches a matching error action', function () {
+      expect(state()).to.equal(err)
+    });
   });
 
   describe('when called without a reducer', function () {
@@ -73,11 +187,11 @@ describe('p.mount', function () {
             res = mount(elm, view)
       root  = res.root
       state = res.state
-      raf(done)
+      expect(state()).to.be.undefined
+      wait(done)
     });
 
     it('defaults the reducer to Identity', function () {
-      expect(state()).to.be.undefined
       root().elm.click()
       expect(state()).to.be.undefined
     });
